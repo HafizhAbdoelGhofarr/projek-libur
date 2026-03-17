df9d32f71d37374ec32e73b3c61c9f44bbb6d17d
HafizhAbdoelGhofarr <haxieaja@gmail.com>
2026-03-18 01:23:59 +0700

    first commit

---
+++ style/respon.js
@@ -0,0 +1,20 @@
+const toggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector("nav ul");
+
+toggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
+
+
+let username;
+let description;
+document.getElementById("submit").onclick = function() {
+
+    username = document.getElementById("name").value;
+    description = document.getElementById("message").value;
+
+    
+    document.getElementById("heroname").textContent = `Hi, I'm ${username}`;
+    document.getElementById("herodesc").textContent = description;
+    console.log(username, description);
+}
